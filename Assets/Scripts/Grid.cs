8913bd533ae697faac9b0c48798968dabc80cf88
mopkaloppt <moppi.sgotmoney@gmail.com>
2022-05-31 11:09:21 +0200

    Implemented Grid
    Draw Grid using Gizmos.DrawCube()
    Check collision by traversing through each node in the grid and use Physics.CheckSphere()

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,8 +10,47 @@ public class Grid : MonoBehaviour
     public float nodeRadius; // how much space each node covers
     Node[,] grid;
 
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+        CreateGrid();
+
+
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        // transfor.position == center
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
+        // Loop through all nodes in grid to check for collision
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
+                grid[x,y] = new Node(walkable, worldPoint);
+            }
+        }
+    }
+
     void onDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x,1,gridWorldSize.y));
+
+        if (grid != null)
+        {
+            foreach (Node n in grid)
+            {
+                Gizmos.color = (n.walkable)? Color.white : Color.red;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+            }
+        }
     }
 }
